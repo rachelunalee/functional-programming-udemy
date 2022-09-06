d1f844d1c4da4e605f653489f9cd1ae8b2c7944e
rachelunalee <rachelunalee@gmail.com>
2022-09-06 15:43:14 -0400

    fp02functional together

--- firstproject/src/main/java/programming/FP02Functional.java
+++ firstproject/src/main/java/programming/FP02Functional.java
@@ -2,13 +2,24 @@
 package programming;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FP02Functional {
 
     public static void main(String[] args) {
         List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+
         int sum = addListStructured(numbers);
         System.out.println(sum);
+
+        System.out.println(doubleList(numbers));
+
+    }
+
+    private static List<Integer> doubleList(List<Integer> numbers) {
+        return numbers.stream()
+                .map(number -> number * number)
+                .collect(Collectors.toList());
     }
 
     private static int sum(int a, int b) {
