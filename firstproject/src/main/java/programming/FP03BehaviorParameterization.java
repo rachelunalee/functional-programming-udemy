6ce77a1fb139a5e69dca11810f72659ca3c2c4a1
rachelunalee <rachelunalee@gmail.com>
2022-09-12 11:59:45 -0400

    behavior parameterization

---
+++ firstproject/src/main/java/programming/FP03BehaviorParameterization.java
@@ -0,0 +1,19 @@
+package programming;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FP03BehaviorParameterization {
+    public static void main(String[] args) {
+        List<Integer> numbers= List.of(12, 9 ,13,4,6,2,4,12,15);
+
+        filterAndPrint(numbers,x->x%2==0);
+        filterAndPrint(numbers,x->x%2!=0);
+    }
+    private static void filterAndPrint(List<Integer> numbers, Predicate<? super Integer> predicate){
+        numbers.stream()
+                .filter(predicate)
+                .forEach(System.out::println);
+    }
+
+}
