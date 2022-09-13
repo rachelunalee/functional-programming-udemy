e61c26b5fc80ac7dfa2a3d54d97f2c1531d44253
rachelunalee <rachelunalee@gmail.com>
2022-09-13 14:31:37 -0400

    takewhile and dropwhile

--- firstproject/src/main/java/programming/FP04CustomClass.java
+++ firstproject/src/main/java/programming/FP04CustomClass.java
@@ -1,6 +1,9 @@
 package programming;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 class Course {
     private String name;
@@ -69,5 +72,33 @@ public class FP04CustomClass {
                 new Course("Kubernetes", "Cloud", 91, 2000)
         );
 
+        Predicate<Course> reviewScoreGreaterThan95Predicate
+                = course -> course.getReviewScore() > 95;
+        Predicate<Course> reviewScoreGreaterThan90Predicate
+                = course -> course.getReviewScore() > 90;
+        Predicate<Course> reviewScoreLessThan90Predicate
+                = course -> course.getReviewScore() < 90;
+
+        System.out.println(courses.stream().allMatch(reviewScoreGreaterThan95Predicate));
+        System.out.println(courses.stream().noneMatch(reviewScoreLessThan90Predicate));
+        System.out.println(courses.stream().anyMatch(reviewScoreLessThan90Predicate));
+        System.out.println(courses.stream().anyMatch(reviewScoreGreaterThan95Predicate));
+
+        Comparator<Course> comparingByNoOfStudents = Comparator.comparing(Course::getNoOfStudents);
+        Comparator<Course> comparingByNoOfStudentsDecreasing = Comparator.comparing(Course::getNoOfStudents).reversed();
+        System.out.println(courses.stream().sorted(comparingByNoOfStudents).collect(Collectors.toList()));
+        System.out.println(courses.stream().sorted(comparingByNoOfStudentsDecreasing).collect(Collectors.toList()));
+
+        System.out.println(
+                courses.stream()
+                        .takeWhile(course -> course.getReviewScore()>=95)
+                        .collect(Collectors.toList())
+        );
+
+        System.out.println(
+                courses.stream()
+                        .dropWhile(course -> course.getReviewScore()>=95)
+                        .collect(Collectors.toList())
+        );
     }
 }
